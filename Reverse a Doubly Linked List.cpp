9ec677b67771b10117b8ba8a2522633a01dbb804
Muskan <124757289+muskan588p@users.noreply.github.com>
2025-03-11 23:49:16 +0530

    Update Reverse a Doubly Linked List.cpp

--- Reverse a Doubly Linked List.cpp
+++ Reverse a Doubly Linked List.cpp
@@ -34,3 +34,23 @@ class Solution {
         return newhead;
     }
 };
+
+// Reverse a Doubly Linked List
+
+// Difficulty: EasyAccuracy: 70.38%Submissions: 170K+Points: 2Average Time: 15m
+// Given a doubly linked list. Your task is to reverse the doubly linked list and return its head.
+
+// Examples:
+
+// Input: LinkedList: 3 <-> 4 <-> 5
+// Output: 5 <-> 4 <-> 3
+
+// Input: LinkedList: 75 <-> 122 <-> 59 <-> 196
+// Output: 196 <-> 59 <-> 122 <-> 75
+
+// Expected Time Complexity: O(n).
+// Expected Auxiliary Space: O(1).
+
+// Constraints:
+// 1 <= number of nodes <= 106
+// 0 <= node->data <= 104
