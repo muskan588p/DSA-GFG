970d89ca8e0dcbe8718e6d3086fe190f354e4a7b
Muskan <124757289+muskan588p@users.noreply.github.com>
2025-04-23 18:12:46 +0530

    Create Shop in Candy Store.cpp

---
+++ Shop in Candy Store.cpp
@@ -0,0 +1,30 @@
+class Solution
+{
+public:
+    vector<int> candyStore(int candies[], int N, int K)
+    {
+        // Write Your Code here
+        sort(candies, candies+N);
+        
+        int min=0;
+        int buy=0;
+        int free=N-1;
+        
+        while(buy<=free){
+            min=min+candies[buy];
+            buy++;
+            free=free-K;
+        }
+        
+        int max=0;
+        buy=N-1;
+        free=0;
+        
+        while(free<=buy){
+            max=max+candies[buy];
+            buy--;
+            free=free+K;
+        }
+        return {min, max};
+    }
+};
