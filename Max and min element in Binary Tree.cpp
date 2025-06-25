5a20995b1dc3c876e9ef34bb49d12768302ab997
Muskan <124757289+muskan588p@users.noreply.github.com>
2025-06-25 15:16:36 +0530

    Update Max and min element in Binary Tree.cpp

--- Max and min element in Binary Tree.cpp
+++ Max and min element in Binary Tree.cpp
@@ -35,3 +35,25 @@ class Solution {
         return min(root->data, min(leftMin, rightMin));
     }
 };
+
+// Max and min element in Binary Tree
+// Difficulty: EasyAccuracy: 60.57%Submissions: 37K+Points: 2
+// Given a Binary Tree, find maximum and minimum elements in it.
+
+// Example:
+
+// Input: 
+           
+// Output: 11 1
+// Explanation: The maximum and minimum element in this binary tree is 11 and 1 respectively.
+// Input: 
+//            6
+//         / \
+//        5   8
+//       /
+//      2
+// Output: 8 2
+// Explanation: The maximum and minimum element in this binary tree is 8 and 2 respectively.
+// Constraints:
+// 1 <= number of nodes <= 105
+// 1 <= data of a node <= 105
