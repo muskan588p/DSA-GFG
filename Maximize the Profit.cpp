6708cb9f562357f9ce7e83b9832064f1bc7cff60
Muskan <124757289+muskan588p@users.noreply.github.com>
2025-06-27 23:07:59 +0530

    Update Maximize the Profit.cpp

--- Maximize the Profit.cpp
+++ Maximize the Profit.cpp
@@ -1,17 +1,18 @@
-You own a candy store in which N different types of candies are available, and each type of candy is kept in a separate jar. Now, given an array candy, where candy[i] denotes the number of candies left in the i'th jar. Suppose, there are K people standing outside your store, and you need to sell exactly one candy to each of them in such a way that your profit is maximum. The cost of each candy is equal to the number of candies left in the jar it belongs to.
+You own a candy store in which N different types of candies are available, and each type of candy is kept in a separate jar. Now, given an array candy, where candy[i] denotes the number of candies left 
+in the i'th jar. Suppose, there are K people standing outside your store, and you need to sell exactly one candy to each of them in such a way that your profit is maximum. The cost of each candy is equal to 
+the number of candies left in the jar it belongs to.
 
 Example 1: N = 3 candy = \{1, 2, 1\} , k = 2
-
-Sell one candy from the middle jar to the first person and earn 2 rupees, then all the jars will contain only one candy, i.e. candy = \{1, 1, 1\} . Now, sell one candy to the second person and earn 1 more rupee. Therefore, the maximum profit earned = 2 + 1 = 3 rupees.
+Sell one candy from the middle jar to the first person and earn 2 rupees, then all the jars will contain only one candy, i.e. candy = \{1, 1, 1\} . Now, sell one candy to the second person and earn 1 more rupee.
+Therefore, the maximum profit earned = 2 + 1 = 3 rupees.
 
 Example 2: N = 2 candy = \{1, 4\} K = 3
 
 To earn maximum profit, sell candies from the second jar only to all the people. The profit would be 4 + 3 + 2 = 9
-
 Input Format
-
 First line of input contain T = number of test cases.
 
 Each test case contains three lines:
 
-First line will contain an integer N, denoting the type of candies available. Second line will contain N space separated integers, denoting the number of canc Third line will contain an integer K, denoting the number of people
+First line will contain an integer N, denoting the type of candies available. Second line will contain N space separated integers, denoting the number of canc Third line will contain an integer K, 
+denoting the number of people
