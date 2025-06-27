bcbd6694ac0cb4066d2d11b197d6b4aae74584d9
Muskan <124757289+muskan588p@users.noreply.github.com>
2025-06-27 23:08:19 +0530

    Update Maximize the Profit.cpp

--- Maximize the Profit.cpp
+++ Maximize the Profit.cpp
@@ -1,3 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int maxCandyProfit(vector<int>& candy, int k) {
+    priority_queue<int> maxHeap;
+
+    for (int c : candy) {
+        maxHeap.push(c);
+    }
+
+    int profit = 0;
+    const int MOD = 1e9 + 7;
+
+    while (k-- && !maxHeap.empty()) {
+        int maxCandies = maxHeap.top();
+        maxHeap.pop();
+        profit = (profit + maxCandies) % MOD;
+
+        if (maxCandies - 1 > 0) {
+            maxHeap.push(maxCandies - 1);
+        }
+    }
+
+    return profit;
+}
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int N;
+        cin >> N;
+
+        vector<int> candy(N);
+        for (int i = 0; i < N; ++i) {
+            cin >> candy[i];
+        }
+
+        int K;
+        cin >> K;
+
+        cout << maxCandyProfit(candy, K) << endl;
+    }
+
+    return 0;
+}
+
 You own a candy store in which N different types of candies are available, and each type of candy is kept in a separate jar. Now, given an array candy, where candy[i] denotes the number of candies left 
 in the i'th jar. Suppose, there are K people standing outside your store, and you need to sell exactly one candy to each of them in such a way that your profit is maximum. The cost of each candy is equal to 
 the number of candies left in the jar it belongs to.
