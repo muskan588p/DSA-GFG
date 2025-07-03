d658f3ba5922717e7df57f1017625bcf46977f90
Muskan <124757289+muskan588p@users.noreply.github.com>
2025-07-03 23:28:08 +0530

    Update Search in Linked List.cpp

--- Search in Linked List.cpp
+++ Search in Linked List.cpp
@@ -16,3 +16,16 @@ class Solution {
         return false;
     }
 };
+// Search in Linked List
+// Difficulty: EasyAccuracy: 65.4%Submissions: 107K+Points: 2Average Time: 10m
+// Given a linked list of n nodes and a key, the task is to check if the key is present in the linked list or not.
+
+// Example:
+
+// Input: n = 4, key = 3
+// 1->2->3->4
+// Output: true
+// Explanation: 3 is present in Linked List, so the function returns true.
+// Constraint:
+// 1 <= n <= 105
+// 1 <= key <= 105
