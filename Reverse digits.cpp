443cc1a840428c11804893a3c721321def4d39a5
Muskan <124757289+muskan588p@users.noreply.github.com>
2025-07-03 23:26:50 +0530

    Update Reverse digits.cpp

--- Reverse digits.cpp
+++ Reverse digits.cpp
@@ -11,3 +11,21 @@ class Solution {
         return rev;
     }
 };
+
+// Reverse digits
+// Difficulty: BasicAccuracy: 46.92%Submissions: 108K+Points: 1
+// You are given an integer n. Your task is to reverse the digits, ensuring that the reversed number has no leading zeroes.
+
+// Examples:
+
+// Input: n = 122
+// Output: 221
+// Explanation: By reversing the digits of number, number will change into 221.
+// Input : n = 200
+// Output: 2
+// Explanation: By reversing the digits of number, number will change into 2.
+// Input : n = 12345 
+// Output: 54321
+// Explanation: By reversing the digits of number, number will change into 54321.
+// Constraints:
+// 1 <= n <= 106
